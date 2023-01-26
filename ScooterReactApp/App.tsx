e69a679052b8b275d5ccfb9e327f3745fc5730be
tkjelds <tkjelds385@gmail.com>
2023-01-26 11:47:55 +0100

    added more styling and linting

--- ScooterReactApp/App.tsx
+++ ScooterReactApp/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,75 +6,92 @@
  * @format
  */
 
-import React, { SetStateAction } from 'react';
 import type {PropsWithChildren} from 'react';
+import React, {SetStateAction} from 'react';
 import {
-  SafeAreaView,
+  GestureResponderEvent,
   Pressable,
+  SafeAreaView,
   ScrollView,
   StatusBar,
   Text,
+  TextInput,
   useColorScheme,
   View,
-  TextInput,
 } from 'react-native';
-
 import {
   DebugInstructions,
   Header,
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import { Scooter } from './Model/Scooter';
+import {Scooter} from './Model/Scooter';
 
 interface EditTextFieldProps {
-  onSearch: React.Dispatch<React.SetStateAction<string>>
-  TextPrompt: string
-  Text: string
+  onSearch: React.Dispatch<React.SetStateAction<string>>;
+  TextPrompt: string;
+  Text: string;
 }
 
 interface LogButtonProps {
-  Title:string
-  LogFunc: Function
+  Title: string;
+  LogFunc: (event: GestureResponderEvent) => void;
 }
-const EditTextField = (Props : EditTextFieldProps) => {
-  return(
-      <TextInput className='bg-slate-200 mx-20 mb-2 rounded-md border-spacing-0 border-dashed border-2 border-purple-900'
+const EditTextField = (Props: EditTextFieldProps) => {
+  return (
+    <TextInput
+      className="bg-slate-200 mx-20 mb-2 rounded-md border-spacing-0 border-2 border-purple-900"
       onChangeText={Props.onSearch}
       placeholder={Props.TextPrompt}
+      placeholderTextColor={'#000'}
       value={Props.Text}
-      />
+    />
   );
-}
+};
 
-const LogButton = (Props:LogButtonProps) => {
-  return(
-    <Pressable onPress={Props.LogFunc} className='bg-purple-700 rounded-sm w-32 h-8 self-center ml-28 border-2 border-purple-900'>
-      <Text className='self-center my-1 border-1 text-white'>{Props.Title}</Text>
+const LogButton = (Props: LogButtonProps) => {
+  return (
+    <Pressable
+      onPress={Props.LogFunc}
+      className="bg-purple-700 rounded-sm w-32 h-8 self-center ml-28 border-2 border-purple-900 border-double">
+      <Text className="self-center my-1 border-1 text-white shadow-black shadow-xl">
+        {Props.Title}
+      </Text>
     </Pressable>
-  )
-}
-
+  );
+};
 
 function App() {
   const [ScooterID, ScooterIDSetter] = React.useState('');
   const [Location, LocationSetter] = React.useState('');
 
   function LogScooter() {
-    if (ScooterID !== "" && Location !== "") {
-      let scoot = new Scooter(ScooterID, Location)
-      console.log(scoot.ToString())
-      ScooterIDSetter("");
-      LocationSetter("");
-    } else { console.log("LMAO"); }
+    if (ScooterID !== '' && Location !== '') {
+      let scoot = new Scooter(ScooterID, Location);
+      console.log(scoot.ToString());
+      ScooterIDSetter('');
+      LocationSetter('');
+    } else {
+      console.log('LMAO');
+    }
   }
 
   return (
-    <SafeAreaView className='py-200 pt-56 pb-96 bg-yellow-300 fill'>
-      <EditTextField TextPrompt={"Enter ScooterID"} onSearch={ScooterIDSetter} Text={ScooterID} />
-      <EditTextField TextPrompt={"Enter Location"} onSearch={LocationSetter} Text={Location} />
-      <LogButton Title={"Log Me!"} LogFunc={LogScooter} />
-    </SafeAreaView>
+    <View className="h-screen w-screen bg-yellow-300 flex justify-center align-middle">
+      <SafeAreaView className="bg-yellow-300 fill">
+        <EditTextField
+          TextPrompt={'Enter ScooterID'}
+          onSearch={ScooterIDSetter}
+          Text={ScooterID}
+        />
+        <EditTextField
+          TextPrompt={'Enter Location'}
+          onSearch={LocationSetter}
+          Text={Location}
+        />
+        <LogButton Title={'Log Me!'} LogFunc={LogScooter} />
+      </SafeAreaView>
+    </View>
   );
 }
 
--- ScooterReactApp/Model/Scooter.tsx
+++ ScooterReactApp/Model/Scooter.tsx
@@ -1,21 +1,21 @@
 export class Scooter {
-    Name : String;
-    Location : String;
-    constructor(Name : String, Location :String) {
-        this.Name = Name;
-        this.Location = Location;
-    }
+  Name: String;
+  Location: String;
+  constructor(Name: String, Location: String) {
+    this.Name = Name;
+    this.Location = Location;
+  }
 
-    ToString() : String {
-        return "Scootername is " + this.Name + " | Location is " + this.Location;
-    }
+  ToString(): String {
+    return 'Scootername is ' + this.Name + ' | Location is ' + this.Location;
+  }
 }
 
 // interface ScooterProps {
 //     Name : String
 //     Location : String
 //   }
-  
+
 // const ScooterToString = (Props : ScooterProps) => {
 //     return "Scootername is " + Props.Name + " | Location is " + Props.Location;
 // }
